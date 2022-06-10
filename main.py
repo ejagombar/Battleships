3e09f562b943b26946fc7da6d573f0a4864efe9b
ejagombar <ejagombar@gmail.com>
2022-06-10 17:58:21 +0100

    finished main game

--- main.py
+++ main.py
@@ -9,41 +9,42 @@ print("main")
 
 class Player():
 
-    
-
     def __init__(self,size,shipNum,name=""):
         self.name = name
         self.size = size
         self.shipNum = shipNum
         self.myGrid = [[0 for x in range(self.size)] for y in range(self.size)]
         self.shipList = []
+        self.destroyedBoatCount = 0
         self.__PlaceShips(self.shipNum)
         
     def Get_myGrid(self):
         return self.myGrid
 
+    def Defeat(self):
+        return (self.destroyedBoatCount == self.shipNum)
+ 
     def Bomb(self,xPos,yPos):
         
         tile = self.myGrid[xPos][yPos]
 
         if (tile == 0) or (tile == -1) or (tile == -2):
-            self.myGrid[xPos][yPos] = -1
+            self.myGrid[xPos][yPos] = -1  #mark miss
             return -1
         else:
             print(tile)
-            self.myGrid[xPos][yPos] = -2
+            self.myGrid[xPos][yPos] = -2  #mark hit
             if self.shipList[int(tile)-1].hitAndDestroyed():
-                return -2
+                self.destroyedBoatCount += 1
+                return -2 #return hit and destroyed
             else:
-                return -3
+                return -3 #return hit
         
-
-
-
     def __PlaceShips(self,shipNum):
         for ship in range(1,shipNum+1):
             
-            shipLength = random.randint(3,5)
+            shipLengthOptions = (2,2,3,4,5)
+            shipLength = shipLengthOptions[ship%len(shipLengthOptions)-1]
             direction = random.randint(0,1)
 
             while True:
@@ -61,13 +62,11 @@ class Player():
 
             shipInArray = Battleship(xpos,ypos,direction,shipLength)
             self.shipList.append(shipInArray)
-            #print(self.shipList[self.shipNum-1].hitAndDestroyed())
+
     def __AddExtraIndent(self):
         if self.size > 10:
             print(" ",end="")
        
-
-
     def DisplayGrid(self,grid,defence):
 
         if self.size > 10:
@@ -117,7 +116,8 @@ class Player():
         self.__AddExtraIndent()
         print(" ",end="")
         print("\N{FULL BLOCK}"*(self.size*2+1))
-        
+
+
 class Battleship():
 
     def __init__(self,xpos,ypos,direction,shipLength):
@@ -143,16 +143,45 @@ class Game():
 
         self.setup()
         self.getplayerNames()
-        self.tutorial(self.player1, self.player2)
-    
+
+        if self.executeTutorial() == 'y':
+            print("")
+            self.tutorial(self.player1, self.player2)
+            self.tutorial(self.player2, self.player1)
+        else:
+            clear()
+
+        self.runMainGame()
+
+    def runMainGame(self):
+
+        while True:
+            self.playerTurn(self.player1,self.player2)
+            self.playerTurn(self.player2,self.player1)
+
+            if self.player1.Defeat():
+                print("\n\n Game Over! "+str(self.player2.name)+ " wins!")
+                break
+            if self.player2.Defeat():
+                print("\n\n Game Over! "+str(self.player1.name)+ " wins!")
+                break
+
+
     def setup(self):
         clear()
+
+        print("\n       /\     |\**/|      \n      /  \    \ == /\n      |  |     |  |\n      |  |     |  |\n     / == \    \  /\n     |/**\|     \/\n")
         print("\N{FULL BLOCK}"*27)
         print("\N{FULL BLOCK} Welcome to Battleships! \N{FULL BLOCK}")
         print("\N{FULL BLOCK}"*27)
         input("\n  Press enter to start")
         clear()
 
+    def executeTutorial(self):
+        result = input("\nWould you like to do the tutorial? (y or n): ")
+        result = str(result[0].lower())
+        return result
+    
 
     def getplayerNames(self):
         
@@ -163,31 +192,67 @@ class Game():
 
     def tutorial(self,player1,player2):
 
-        input("\n It is "+str(player1.name)+"'s go. Press enter to start")
+        input("It is "+str(player1.name)+"'s go. Press enter to start")
         clear()
 
+        print("Here is your battleship map. You have "+str(player1.shipNum)+" battleships")
+        print("Do not give away your battleship locations to "+str(player2.name)+"!")
         player1.DisplayGrid(player1.Get_myGrid(),True)
-        print("\nHere is your battleship map. You have "+str(player1.shipNum)+" battleships")
-        input("Do not give away your battleship locations to "+str(player2.name))
-
-        self.player2.DisplayGrid(player2.Get_myGrid(),False)
+        input("\n Press enter to continue")
+        
         print("\nHere is " + str(player2.name) + "'s map.")
         print("Their ships do not appear on your radar however they need to be destroyed.")
-        print("Chose a location on the grid to send a missile\n")
+        print("Chose a location on the grid to send a missile")
+        player2.DisplayGrid(player2.Get_myGrid(),False)
 
-        self.playerShoot(player2)
+        result = self.playerShoot(player2)
 
-        input("\n Press enter to continue")
+        clear()
 
-
-
-    def playerShoot(self,player):
-        xInput = int(input("X Coordinate: "))
-        yInput = int(input("Y Coordinate: "))
-
-        result = player.Bomb(yInput,xInput)
+        self.player2.DisplayGrid(player2.Get_myGrid(),False)
 
         print("")
+
+        self.DisplayResult(result)
+
+        input("\n Continue")
+        clear()
+
+    def playerTurn(self,player1,player2):
+
+        input("It is "+str(player1.name)+"'s go. Press enter to start")
+        clear()
+
+        print(str(player1.name)+"'s move:\n")
+
+        print("---Your Battleships---\n")
+        player1.DisplayGrid(player1.Get_myGrid(),True)
+
+        print("\n\n---Enemy Battleships---\n")
+        player2.DisplayGrid(player2.Get_myGrid(),False)
+
+        print("Enter your missile coordinates")
+
+        result = self.playerShoot(player2)
+
+        clear()
+
+        print(str(player1.name)+"'s move:\n")
+
+        print("---Your Battleships---\n")
+        player1.DisplayGrid(player1.Get_myGrid(),True)
+
+        print("\n\n---Enemy Battleships---\n")
+        player2.DisplayGrid(player2.Get_myGrid(),False)
+
+        print("")
+        self.DisplayResult(result)
+
+        input("\nEnter to end go")
+        clear()
+
+
+    def DisplayResult(self,result):
 
         if result == -1:
             print("Missed!")
@@ -195,6 +260,18 @@ class Game():
             print("Hit!")
         elif result == -2:
             print("Hit and Destroyed!")
+            print("\n                __/___            \n         _____/______|           \n _______/_____\_______\_____     \n \              < < <       |    \n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+    def playerShoot(self,player):
+
+        print("\n")
+        xInput = int(input("X Coordinate: "))
+        yInput = int(input("Y Coordinate: "))
+
+        result = player.Bomb(yInput,xInput)
+
+        return result
 
 
 game = Game()
